886534ff6eea97a8a90f2440c717d206258c3ebd
Habib-Ashraf-Hassan <100794988+Habib-Ashraf-Hassan@users.noreply.github.com>
2023-10-30 16:26:01 +0300

    increased max size of spring in newly added .py

--- candy_dispenser_user.py
+++ candy_dispenser_user.py
@@ -100,7 +100,7 @@ class CandyDispenserApp:
             self.info_label.config(text="Cannot POP; Candy dispenser is empty", fg="red")
 
     def update_dispenser(self):
-        spring_dynamic_height = 160 - 10*(self.candy_colors.size())
+        spring_dynamic_height = 200 - 10*(self.candy_colors.size())
         self.candy_dispenser_frame.destroy()
 
         self.candy_dispenser_frame = tk.Frame(self.root)
